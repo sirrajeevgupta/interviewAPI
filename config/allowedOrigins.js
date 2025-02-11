19ea898ebe889f5253990ce5b1c05a937c610eb1
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-02-11 22:58:14 +0530

    correcting frontend website name

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,7 @@ const allowedOrigins = [
   'http://localhost:3500',
   'http://localhost:3000',
   'https://interviewapi.onrender.com',
-  'https://interview.onrender.com',
+  'https://interview-01mp.onrender.com',
 ];
 
 module.exports = allowedOrigins;
